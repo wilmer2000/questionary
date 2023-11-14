6d600f8bf84733e6c0f8c6aebf2ba09293326e7c
Wilmer Blanco <wilmervzla2000@gmail.com>
2023-11-14 15:58:54 -0300

    prisma installation in progress

---
+++ api/src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../shared/prisma/prisma.service';
+import { Prisma, User } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+}
